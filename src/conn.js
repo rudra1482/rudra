d78c1e746e718a7e790957045954205c9fba15b0
rudra1482 <140492507+rudra1482@users.noreply.github.com>
2023-07-25 13:41:52 +0530

    Create conn.js

---
+++ src/conn.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+const conn_string = process.env.CONN_STRING;
+
+mongoose.connect(conn_string , {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ autoIndex: true, 
+}).then(()=>{
+  console.log('connected to database!!')
+}).catch((e)=>{
+  console.log(`No connection ${e}`)
+});
